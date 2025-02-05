8584c5600b3b778a938e609d10125cdc1d888964
RobertSajdak <robert.sajdak@onet.eu>
2025-02-05 10:03:17 +0100

    bugs fixed

--- src/App.jsx
+++ src/App.jsx
@@ -5,41 +5,29 @@ import UserInput from './components/UserInput.jsx';
 import Results from './components/Results.jsx';
 
 function App() {
-	// Stan początkowy, czyli dane wejściowe użytkownika.
-	const [userInput, setUserInput] = useState({
-		initialInvestment: 10000,
-		annualInvestment: 1200,
-		expectedReturn: 6,
-		duration: 10,
-	});
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
 
-	const inputIsValid = userInput.duration >= 1;
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: +newValue,
+      };
+    });
+  }
 
-	// Funkcja do obsługi danych wejściowych (pól <input>), których wartość zmienił użytkownik.
-	// Funkcja aktualizuje stan pól po każdej zmianie wartości.
-	function handleChange(inputIdentifier, newValue) {
-		setUserInput(prevUserInput => {
-			return {
-				...prevUserInput,
-				[inputIdentifier]: +newValue, // + wymusza tutaj konwersję String'a na typ number
-			};
-		});
-	}
-
-	return (
-		<>
-			<Header />
-			<UserInput userInput={userInput} onChange={handleChange} />
-			{!inputIsValid && (
-				<p className="center">
-					<b>Warnning!</b>
-					<br />
-					The value of the duration field must be greater than zero.
-				</p>
-			)}
-			{inputIsValid && <Results input={userInput} />}
-		</>
-	);
+  return (
+    <>
+      <Header />
+      <UserInput userInput={userInput} onChange={handleChange} />
+      <Results input={userInput} />
+    </>
+  );
 }
 
 export default App;
--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,23 +1,29 @@
 import { calculateInvestmentResults, formatter } from '../util/investment.js';
 
 export default function Results({ input }) {
-	const resultsData = calculateInvestmentResults(input);
+	const results = [];
+	calculateInvestmentResults(input, results);
+
+	if (results.length === 0) {
+		return <p className="center">Invalid input data provided.</p>;
+	}
+
 	const initialInvestment =
-		resultsData[0].valueEndOfYear - resultsData[0].interest - resultsData[0].annualInvestment;
+		results[0].valueEndOfYear - results[0].interest - results[0].annualInvestment;
 
 	return (
 		<table id="result">
 			<thead>
 				<tr>
-					<th>Year</th> {/*Rok inwestycji*/}
-					<th>Investment Value</th> {/*Wartość inwestycji*/}
-					<th>Interest (Year)</th> {/*Odsetki w danym roku*/}
-					<th>Total Interest</th> {/*Suma odsetek za wszystkie lata inwestycji*/}
-					<th>Invested Capital</th> {/*Zainwestowany kapitał łącznie*/}
+					<th>Year</th>
+					<th>Investment Value</th>
+					<th>Interest (Year)</th>
+					<th>Total Interest</th>
+					<th>Invested Capital</th>
 				</tr>
 			</thead>
 			<tbody>
-				{resultsData.map(yearData => {
+				{results.map(yearData => {
 					const totalInterest =
 						yearData.valueEndOfYear -
 						yearData.annualInvestment * yearData.year -
